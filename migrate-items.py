08bca02e1f42ac8edea651fdcbf46a95125fdd8f
Sarina <ssinick@calstate.edu>
2016-11-11 00:58:57 -0800

    Added complete field mapping to item record csv file link, using expected field name mapping

--- migrate-items.py
+++ migrate-items.py
@@ -28,15 +28,18 @@ def get_sru_base():
 	
 def get_base_url():
 	return config.get('Params', 'baseurl')
-
+	
+def get_field_mapping():
+	return config.get('Params', 'fieldmap')
 
 """
 	Set up authoritative mapping:
 		the mapping between the 'expected' alma fields and the API fields that correspond. 
 		This can be done in a static way, it won't change, only the mapping between the expected and the local field name is dynamic
 """
-def create_authoritative_mapping():
+def get_authoritative_mapping():
 	dict = {'VOLUME':'description',
+			'035|a': 'oclc',
 			'COPY #':'copy_id', #holding
 			'BARCODE':'barcode',
 			'LOCATION':'location',
@@ -70,7 +73,7 @@ def read_mapping(mapping_file):
 		reader.next() #skip header line
 		for row in reader:
 			if row[1]:
-				field_mapping[row[0].strip()] = row[1].strip()
+				field_mapping[row[1].strip()] = row[0].strip()
 		return field_mapping
 	finally:
 		f.close()
@@ -128,6 +131,39 @@ def read_itype_mapping(itype_map_file):
 
 
 """
+
+"""
+def set_fields(indices,row):
+	if 'LOCATION' in indices:
+		location = row[indices['LOCATION']]
+	if 'BARCODE' in indices:
+		barcode = row[indices['BARCODE']]
+	if 'I TYPE' in indices:
+		itype = row[indices['I TYPE']]
+	if 'STATUS' in indices:
+		status = row[indices['STATUS']]
+	if 'CALL #(BIBLIO)' in indices:
+		call_number = row[indices['CALL #(BIBLIO)']]
+	if 'CREATED(ITEM)' in indices:
+		created = row[indices['CREATED(ITEM)']]
+	if 'UPDATED(ITEM)' in indices:
+		updated = row[indices['UPDATED(ITEM)']]
+	if 'FULFILMENT_NOTE' in indices:
+		ful_note_1 = row[indices['FULFILMENT_NOTE']]
+	if 'STAT_NOTE_1' in indices:				
+		stat_note_1 = row[indices['STAT_NOTE_1']]
+	if 'STAT_NOTE_2' in indices:
+		stat_note_2 = row[indices['STAT_NOTE_2']]
+	if 'STAT_NOTE_3' in indices:
+		stat_note_3 = row[indices['STAT_NOTE_3']]
+	if 'NON_PULIC_NOTE_1' in indices:
+		int_note_1 = row[indices['NON_PUBLIC_NOTE_1']]
+	if 'NON_PULIC_NOTE_2' in indices:
+		int_note_2 = row[indices['NON_PUBLIC_NOTE_2']]
+	if 'NON_PULIC_NOTE_3' in indices:
+		int_note_3 = row[indices['NON_PUBLIC_NOTE_3']]
+
+"""
 	Read in item data in csv format (item_data.csv)
 """
 def read_items(item_file):
@@ -135,18 +171,22 @@ def read_items(item_file):
 	try:
 		reader = csv.reader(f)
 		header = reader.next()
+		indices = map_headers(header)
 		for row in reader:
 			oclc = '08387229' #row[1]
-			# need to do an index to column label mapping here! 
-			location = row[7]
-			# Get bib record MMS ID, do something about multi-match. 
+			# Check if bib record exists, based on OCLC number.  If it exists, get mms id  
 			mms_id = find_mms_id(oclc)
 			print mms_id
+			if 'LOCATION' in indices:
+				location = row[indices['LOCATION']]
+			# Check if holding with same location as item exists.  If so, proceed to attach item
 			holding_id = check_for_holdings(mms_id,location)
+			# If holding doesn't already exist, create holding, then proceed to attach items
 			if holding_id == 0:
 				make_holding(mms_id)
 			# create_item()
 			print holding_id
+			set_fields(indices,row)
 	finally:
 		f.close()
 
@@ -156,10 +196,26 @@ def read_items(item_file):
 
 
 """
+	Maps authoritative headers to indices	
+"""
+def map_headers(header):
+	index_map = {}
+	field_map = read_mapping(get_field_mapping())
+	print(field_map)
+	position = 0
+	for column in header:
+		if column in field_map:
+			index_map[field_map[column]] = position
+		position += 1
+	print(index_map)
+	return index_map
+		
+
+"""
 	Check if holding record exists
 """
 def check_for_holdings(bib_mms_id,loc):
-	holding_url = get_base_url() + '/bibs/' + bib_mms_id + '/holdings?apikey=' + get_key()
+	holding_url = get_base_url() + '/bibs/' + str(bib_mms_id) + '/holdings?apikey=' + get_key()
 	response = requests.get(holding_url)
 	holdings = ET.fromstring(response.content)
 	if response.status_code == 200:
@@ -205,7 +261,7 @@ def find_mms_id(oclc):
 """
 
 # set these in config file I think
-#mapping_file = sys.argv[1]
+mapping_file = sys.argv[3]
 #location_file = sys.argv[2]
 #status_file = sys.argv[3]
 #itype_file = sys.argv[4]
